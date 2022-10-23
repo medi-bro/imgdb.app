22d5e491fb385b55239b44980382908435370e4d
Jeremy <sirjeremycortez@gmail.com>
2022-10-22 20:31:17 -0700

    Created LoginButton and added it to nav bar

---
+++ src/components/logins/LoginButton.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { auth } from "../../firebase-access";
+import { signOut, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+
+export const LoginButton = () => {
+    const navigate = useNavigate();
+
+    const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+    const [buttonLabel, setButtonLabel] = useState("Sign In");
+
+    const handleSignIn = () => {
+        navigate("/login");
+    }
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                console.log("User signed out.");
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                navigate("/");
+            });
+    };
+
+    const handleOnClick = () => {
+        if(isUserSignedIn) {
+            handleSignOut();
+        }
+        else {
+            handleSignIn();
+        }
+    };
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            if(currentUser) {
+                setIsUserSignedIn(true);
+                setButtonLabel("Log Out");
+            }
+            else {
+                setIsUserSignedIn(false);
+                setButtonLabel("Log In");
+            }
+        });
+    }, [])
+
+    return(<Button id="login-button" onClick={handleOnClick} variant="contained">{buttonLabel}</Button>);
+};
