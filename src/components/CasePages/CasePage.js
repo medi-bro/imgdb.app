1ef32a1840a7d70b660de84acc9cf54df091ffa1
Jose Gutierrez <jg16407@gmail.com>
2022-11-11 21:30:02 -0800

    Added css and restructured code in CasePage

--- src/components/CasePages/CasePage.js
+++ src/components/CasePages/CasePage.js
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import { useNavigate } from 'react-router-dom';
-import { db } from "../../firebase-access";
-import { Button } from "@mui/material";
-import { auth } from "../../firebase-access";
+import { Card, CardActions, CardContent, Typography} from "@mui/material";
+import { db, auth } from "../../firebase-access";
 import { onAuthStateChanged } from "firebase/auth"; 
 import { CaroScans } from "./CarouselScans";
 import "./CasePage.css";
@@ -15,33 +14,94 @@ export const CasePage = () => {
     let navigate = useNavigate();
     const [userDocs, getUserDocs] = useState([]); 
     const [cases, getCases] = useState([]);
-    let   [flag, setFlag] = useState(0);
+    let   [flag, setFlag] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const fetchUserCases = async (uID) =>{
-        const documentSnapshot = await getDoc(uID);
+    const fetchUserCases = async (userRef) => {
+        const documentSnapshot = await getDoc(userRef);
         let caseArr = [];
         if (documentSnapshot.exists() && documentSnapshot.data().cases !== undefined){
             caseArr = documentSnapshot.data().cases;
             getUserDocs([...caseArr]);
         }
         if(caseArr.length === 0){
-            setFlag(1); 
+            setFlag(true);
+            setIsLoading(false);  
         }else{
             const data = await getDocs(collection(db, "cases"));
             getCases(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+            setIsLoading(false); 
         }
     }
 
+    const DisplayCases = () => {
+        return(
+            cases.map((caseDoc) => {
+            for(let i = 0; i <= userDocs.length; i++){
+              if(caseDoc.id === userDocs[i]){
+                try{
+                    var date = new Date(caseDoc.date.seconds * 1000).toLocaleString('en-US');
+                }catch{ 
+                    console.log("No date timestamp for " + caseDoc.id);
+                }
+                return (
+                    <div key = {caseDoc.id}>
+                        <Card sx={{ width: 550, boxShadow: 15 }} className = "cards">
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                    Case ID: {caseDoc.id}
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    {caseDoc.title}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    Physician: {caseDoc.doctorId}
+                                </Typography>
+                                <Typography sx={{ mb: -1 }}>
+                                    Date: {date}
+                                </Typography>
+                            </CardContent>
+                            <CardActions sx= {{justifyContent: 'center'}}>
+                                <button id = {caseDoc.id} onClick={routeCase} variant = "outlined" className="button">
+                                    <span>
+                                        Learn More
+                                    </span>
+                                </button>
+                            </CardActions>
+                        </Card>
+                        <br/>  
+                    </div>
+                );
+              }
+            }
+          return null;
+          })
+        )
+    }
+
+    const NoCases = () => {
+        return(
+            <Card sx={{ boxShadow: 15 }} className = "cards">
+                <CardContent>
+                    <Typography sx={{ fontSize: 20, fontWeight: 'bold'}}>
+                        No cases to display
+                    </Typography>
+                </CardContent>
+            </Card>
+        )
+    }
+
     const routeCase = event => {
-        //console.log(event.currentTarget.id);
-        navigate("/*", {state:{ caseName: event.currentTarget.id}});
+        console.log(event.currentTarget.id);
+        navigate("/*", {/*state:{ caseName: event.currentTarget.id}*/});
     };
 
-    useEffect(() => { 
+    useEffect(() => {
         onAuthStateChanged(auth, (currentUser) => {
             if(currentUser){
-                const userID = doc(db, "users", currentUser.uid);
-                fetchUserCases(userID);
+                const userRef = doc(db, "users", currentUser.uid);
+                fetchUserCases(userRef);
+                setIsLoading(true); 
             }else{
                 navigate("/login"); 
             }
@@ -50,30 +110,9 @@ export const CasePage = () => {
 
     return (
         <div className = "mainDiv">
-        <h1 className = "header" >Cases:</h1>
-        {flag? <h2>No Cases to display</h2>  : cases.map((caseDoc) => {
-            for(let i = 0; i <= userDocs.length; i++){
-              if(caseDoc.id === userDocs[i]){
-                try{
-                    var date = new Date(caseDoc.date.seconds * 1000).toLocaleString('en-US');
-                }catch{ 
-                    console.log("No date timestamp found for " + caseDoc.id);
-                }
-                return (
-                    <div key = {caseDoc.id} className = "buttonDiv">
-                    <Button variant="contained" id = {caseDoc.id} onClick={routeCase} className = "button"
-                    style={{ fontSize: '16px' }} >
-                    Case: {caseDoc.title} <br/>
-                    Date: {date} 
-                    </Button>
-                    </div>
-                );
-              }
-            }
-          return null;
-          })
-        }
-        <CaroScans/> 
+            <h1 className = "header">Cases</h1>
+            {isLoading? <div className="loading"/> : (flag? <NoCases/> : <DisplayCases/>)}
+            <CaroScans/> 
         </div>
     );
 }
