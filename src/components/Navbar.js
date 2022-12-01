4aafc691f1e93955d8905068bb60fb85140fc70a
Jeremy Cortez <sirjeremycortez@gmail.com>
2022-11-30 23:40:33 -0800

    Renamed images to lowercase png to fixe case issue in deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ export const Navbar = () => {
   return (
     <nav className="nav">
       <a href="/" className="Logo-space">
-        <img className="Logo" src="/imgs/md_logo.png" alt=""></img>
-        <img className="title" src="/imgs/MD_title.png" alt=""></img>
+        <img className="Logo" src="/imgs/md_logoa.png" alt=""></img>
+        <img className="title" src="/imgs/MD_titlea.png" alt=""></img>
       </a>
       <ul className="Navbar-links">
         <li>
